b52d6f2d652fc27897c6509ab9c05224d5c23440
Alex Cline <acline@us.ibm.com>
2013-01-31 14:02:49 -0500

    Updated script to populate serial numbers into AD if available and to provide verbose logging for quarterly reports.

--- vadar.rb
+++ vadar.rb
@@ -3,47 +3,231 @@ require 'rubygems'
 require 'bundler/setup'
 require 'net/ldap'
 require 'net/http'
+require 'net/smtp'
 require 'json'
+require 'awesome_print'
 
 # Load the file with BIND credentials: BIND_USER & BIND_PASS
 env_vars = File.join(Dir.pwd, 'env_vars.rb')
 load(env_vars) if File.exists?(env_vars)
 
-host = "corp.vivisimo.com"
-port = "389"
-basedn = "dc=corp,dc=vivisimo,dc=com"
-w3url = "http://bluepages.ibm.com/BpHttpApisv3/slaphapi?"
+action = ARGV[0]
+$accounts = []
+$verbose  = false
 
-scope = Net::LDAP::SearchScope_WholeSubtree
-filter = "(&(objectClass=person)(!(objectClass=computer))(!(userAccountControl:1.2.840.113556.1.4.803:=2)))"
-attrs = ['displayName','sAMAccountName','dn','mail']
+def corp_lookup
+  basedn = "cn=users,dc=corp,dc=vivisimo,dc=com"
+  scope = Net::LDAP::SearchScope_WholeSubtree
+  filter = "(&(objectClass=person)(!(objectClass=computer))(!(userAccountControl:1.2.840.113556.1.4.803:=2)))"
+  attrs = ['displayName','sAMAccountName','dn','mail', 'serialNumber']
 
+  ldap = Net::LDAP.new
+  ldap.host = "corp.vivisimo.com"
+  ldap.port = "389"
+  ldap.auth ENV['BIND_USER'], ENV['BIND_PASS']
+  
+  ldap.search(:base => basedn, :scope => scope, :filter => filter, :attributes => attrs, :return_result => true) do |entry|
 
-ldap = Net::LDAP.new
-ldap.host = host
-ldap.port = port
-ldap.auth ENV['BIND_USER'], ENV['BIND_PASS']
+    account = { 
+      :id   => entry.sAMAccountName.first.to_s, 
+      :dn   => entry.dn.to_s,
+      :msg  => "",
+      :mail => "",
+      :sn   => "",
+    }
 
-ldap.search(:base => basedn, :scope => scope, :filter => filter, :attributes => attrs, :return_result => true) do |entry|
-
-  if entry.respond_to? :mail
-    uri = URI("#{w3url}ibmperson/mail=#{entry.mail.first}.list/byjson")
-    json = Net::HTTP.get(uri)
-    data = JSON.parse(json)
-
-    if data["search"]["return"]["count"] == 0
-      puts "No active account in BluePages for email #{entry.mail.first.to_s} for user #{entry.dn.to_s}"
+    if $verbose 
+      account[:msg] += "#{account[:id]}: DN: #{account[:dn]}\n"
     end
 
-    if data["search"]["return"]["count"] > 1
-      puts "More than one result in BluePages for #{entry.mail.first.to_s}"
+    if entry.respond_to? :mail
+      account[:mail] = entry.mail.first.to_s
+      if $verbose
+        account[:msg] += "#{account[:id]}: mail: #{account[:mail]}\n"
+      end
+    else
+      account[:msg]  += "#{account[:id]}: No email address in AD\n"
+      account[:needsEmail] = true
     end
 
-    #data["search"]["entry"][0]["attribute"].each { |attr| attr["name"] == "ibmserialnumber" ? (puts attr["value"]) : nil }
-  else
+    if entry.respond_to? :serialNumber
+      account[:sn] = entry.serialNumber.first.to_s
+      if $verbose
+        account[:msg] += "#{account[:id]}: sn: #{account[:sn]}\n"
+      end
+    else
+      account[:msg] += "#{account[:id]}: No serial number in AD\n"
+      account[:needsSerialNumber] = true
+    end
 
-    puts "No Email address in AD for #{entry.dn.to_s}"
+    $accounts.push account
+  end
+end
+
+def bluepages_lookup
+  w3url = "http://bluepages.ibm.com/BpHttpApisv3/slaphapi?"
+
+  $accounts.each do |account|
+
+    if account[:sn] != ""
+      if $verbose
+        account[:msg] += "#{account[:id]}: BP lookup with sn: #{account[:sn]}\n"
+      end
+      uri = URI("#{w3url}ibmperson/serialnumber=#{account[:sn].to_s}.list/byjson")
+      json = Net::HTTP.get(uri)
+      data = JSON.parse(json)
+
+      if data["search"]["return"]["count"] == 0
+        account[:msg] += "#{account[:id]}: No account in BP with sn: #{account[:sn]}\n"
+        account[:hardfail] = true
+        next
+      end
+
+      attributes = data["search"]["entry"].first["attribute"]
+      attributes.each do |attr|
+        if attr["name"] == "mail"
+          if !account[:needsEmail] && attr["value"].first != account[:mail]
+            account[:msg] += "#{account[:id]}: Mail in BP doesn't match Mail in AD [#{attr["value"].first} != #{account[:mail]}]\n"
+            account[:updatemail] = true
+          elsif attr["value"].first == account[:mail]
+            account[:msg] += "#{account[:id]}: Account in AD is valid\n"
+          end
+        end
+      end
+
+      next
+    end
+
+    if account[:mail] != ""
+      if $verbose
+        account[:msg] += "#{account[:id]}: BP lookup with mail: #{account[:mail]}\n"
+      end
+      uri = URI("#{w3url}ibmperson/mail=#{account[:mail].to_s}.list/byjson")
+      json = Net::HTTP.get(uri)
+      data = JSON.parse(json)
+
+      if data["search"]["return"]["count"] == 0
+        if account[:needsSerialNumber]
+          account[:msg] += "#{account[:id]}: Please manually check BluePages to see if this user has changed their primary internet address.\n"
+        end
+        account[:msg] += "#{account[:id]}: No account in BP with mail: #{account[:mail]}\n"
+        account[:hardfail] = true
+        next
+      end
+
+      attributes = data["search"]["entry"].first["attribute"]
+      attributes.each do |attr|
+        if attr["name"] == "serialnumber"
+          if !account[:needsSerialNumber] && attr["value"].first != account[:sn]
+            account[:msg] += "#{account[:id]}: SN in BP doesn't match SN in AD [#{attr["value"].first} != #{account[:sn]}]\n"
+            account[:updatesn] = true
+          elsif attr["value"].first == account[:sn]
+            account[:msg] += "#{account[:id]}: Account in AD is valid\n"
+          elsif account[:needsSerialNumber]
+            account[:sn] = attr["value"].first
+            account[:msg] += "#{account[:id]}: Will update SN in AD\n"
+          end
+        end
+      end
+    end
+
+  end
+end
+
+def corp_save
+  ldap = Net::LDAP.new
+  ldap.host = "corp.vivisimo.com"
+  ldap.port = "389"
+  ldap.auth ENV['BIND_USER'], ENV['BIND_PASS']
+
+
+  $accounts.each do |account|
+    ops = []
+
+    if account[:hardfail]
+      next
+    end
+
+    if account[:needsSerialNumber]
+      ops.push [:replace, :serialNumber, account[:sn]]
+      account[:msg] += "#{account[:id]}: Updating SN in AD to #{account[:sn]}\n"
+    end
+
+    if account[:needsEmail]
+      ops.push [:replace, :mail, account[:mail]]
+      account[:msg] += "#{account[:id]}: Updating mail in AD to #{account[:mail]}\n"
+    end
+
+    if account[:needsSerialNumber] || account[:needsEmail]
+      result = ldap.modify :dn => account[:dn], :operations => ops
+      if result == true
+        account[:msg] += "#{account[:id]}: AD update successful\n"
+      else
+        account[:msg] += "#{account[:id]}: AD update unsuccessful: #{result.inspect}\n"
+      end
+    end
+  end
+end
+
+def send_mail verbose, tagline
+  failures = false
+  server  = "localhost"
+  to      = "im-bigdata-pgh-sysadmins@wwpdl.vnet.ibm.com"
+  from    = "VADAR <acline@us.ibm.com>"
+  subject = "Vigilant Active Directory Auditor Results"
+  head    = ""
+  body    = ""
+  details = ""
+
+  head << "From: #{from}\n"
+  head << "To: #{to}\n"
+  head << "Subject: #{subject}\n"
+
+  body << "#{tagline}  Please review the data below and take necessary steps.\n\n"
+
+  $accounts.each do |account|
+    if verbose
+      body << "#{account[:msg]}\n"
+    end
+
+    if account[:hardfail]
+      failures = true
+      body << "A problem was detected with account: #{account[:id]}\n"
+      body << "  #{account[:msg].split(/\r?\n/).last}\n"
+      details << "#{account[:msg]}\n\n"
+    end
   end
 
+  if failures
+    body << "\n\nDetails:\n\n#{details}"
+  else
+    body << "There were no account issues detected this run."
+  end
 
+  Net::SMTP.start(server, 25) do |smtp|
+    message = "#{head}\n#{body}\n"
+    res = smtp.send_message message, to, from
+    smtp.finish
+  end
+end
+
+
+if action == "daily_audit"
+  corp_lookup
+  bluepages_lookup
+  send_mail false, "This is the daily audit summary."
+elsif action == "quarterly_report"
+  $verbose = true
+  corp_lookup
+  bluepages_lookup
+  send_mail false, "This is the quarterly audit report."
+elsif action == "test"
+  $verbose = true
+  corp_lookup
+  bluepages_lookup
+  corp_save
+  
+  $accounts.each do |account|
+    puts account[:msg]
+  end
 end
