e1fb7047949ea9909f93c0323fc2d769873c34c6
Alex Cline <acline@us.ibm.com>
2013-01-31 14:46:15 -0500

    Oops.  Forgot file extension.

--- daily_audit.sh
+++ daily_audit.sh
@@ -2,4 +2,4 @@ SHELL=/bin/bash
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 MAILTO=acline@us.ibm.com
 HOME=/root
-0 12 * * *     cd /opt/vadar; rvm use 1.9.3; ruby vadar daily_audit
+0 12 * * *     cd /opt/vadar; rvm use 1.9.3; ruby vadar.rb daily_audit
--- quarterly_report.sh
+++ quarterly_report.sh
@@ -2,4 +2,4 @@ SHELL=/bin/bash
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 MAILTO=acline@us.ibm.com
 HOME=/root
-0 12 1 1,4,7,10 *     cd /opt/vadar; rvm use 1.9.3; ruby vadar quarterly_report
+0 12 1 1,4,7,10 *     cd /opt/vadar; rvm use 1.9.3; ruby vadar.rb quarterly_report
