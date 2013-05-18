e073737de59185f4127d47aeeb1f7aaaa3c1c786
Alex Cline <alex.cline@gmail.com>
2013-05-18 02:58:25 -0400

    More methods for the ad class.

--- lib/ad.rb
+++ lib/ad.rb
@@ -18,42 +18,65 @@ class Ad
   end
 
   def connected?
-    begin
-      @ldap.bind
-    rescue Exception => e
-      raise "Error connecting to AD server. #{e}"
+    unless @ldap.bind
+      raise "Error connecting to AD server. \
+#{@ldap.get_operation_result.code}: \
+#{@ldap.get_operation_result.message}"
     end
+  end
+
+  def setManager id, manager
+    return modify(getDN(id), [:manager, getDN(manager)])
+  end
+
+  def setMail id, mail
+    return modify(getDN(id), [:mail, mail])
+  end
+
+  def setSerial id, serial
+    return modify(getDN(id), [:serialNumber, serial])
+  end
+
+  def getDN id
+    return search id, :dn
   end
 
   def getManager id
-    connected?
-    options = @options.merge({:filter => userFilter(id), :attributes => ['manager']})
-    @ldap.search(options) do |entry|
-      return entry.respond_to?(:manager) ? entry.manager.first.to_s : nil
-    end
+    dn = search id, :manager
   end
 
   def getMail id
-    connected?
-    options = @options.merge({:filter => userFilter(id), :attributes => ['mail']})
-    @ldap.search(options) do |entry|
-      return entry.respond_to?(:mail) ? entry.mail.first.to_s : nil
-    end
-    raise "#{id}: Couldn't find user with the requested id"
+    return search id, :mail
   end
 
   def getSerial id
-    connected?
-    options = @options.merge({:filter => userFilter(id), :attributes => ['serialNumber']})
-    @ldap.search(options) do |entry|
-      return entry.respond_to?(:serialNumber) ? entry.serialNumber.first.to_s : nil
-    end
-    raise "#{id}: Couldn't find user with the requested id"
+    return search id, :serialNumber
   end
 
-  def userFilter sAMAccountName
-    configuration.config['ad']['filter'].insert(-2,
-      "(sAMAccountName=#{sAMAccountName})")
+  def getId dn
+    return search dn, :sAMAccountName, "dn"
+  end
+
+  def modify dn, opts
+    connected?
+    result = @ldap.modify(:dn => dn, :operations => [[:replace].concat(opts)])
+    raise "Error modifying #{opts} of #{dn}. \
+#{@ldap.get_operation_result.code}: \
+#{@ldap.get_operation_result.message}" if result != true
+    return result
+  end
+
+  def search id, obj, field="sAMAccountName"
+    opts = @options.merge({:filter => makeFilter(id, field), :attributes => [obj.to_s]})
+    connected?
+    @ldap.search(opts) do |entry|
+      return entry.respond_to?(obj) ? entry[obj].first.to_s : nil
+    end
+    raise "Couldn't find user with id #{id} using #{opts}"
+  end
+
+  def makeFilter id, field
+    configuration.config['ad']['filter'].sub("VALUE", id).sub("FIELD", field)
   end
 
 end
--- spec/lib/ad_spec.rb
+++ spec/lib/ad_spec.rb
@@ -4,57 +4,108 @@ require 'ad'
 
 describe Ad do
   before :each do
-    @lookup = Ad.new
+    @ad = Ad.new
   end
 
   describe ".connected?" do
-    it "raises and error when the server could not be contacted" do
-      expect { @lookup.connected? }.to raise_error
+    xit "raises and error when the server could not be contacted" do
+      expect { @ad.connected? }.to raise_error
+    end
+  end
+
+  describe ".getId" do
+    it "returns an id when passed a DN" do
+      @ad.getId("CN=Alex Cline,CN=Users,DC=bigdatalab,DC=ibm,DC=com").should
+        eql "cline"
+    end
+  end
+
+  describe ".setSerial" do
+    it "returns true if the serial was set successfully" do
+      @ad.setSerial("cline", "1G4959897").should eql true
+    end
+
+    it "raises an error if it couldn't find the user" do
+      expect{ @ad.setSerial("falseuser", "0000")}.to raise_error
+    end
+  end
+
+  describe ".setMail" do
+    it "returns true if the serial was set successfully" do
+      @ad.setMail("cline", "acline@us.ibm.com").should eql true
+    end
+
+    it "raises an error if it couldn't find the user" do
+      expect{ @ad.setMail("falseuser", "f@example.com")}.to raise_error
+    end
+  end
+
+  describe ".setManager" do
+    it "returns true if the serial was set successfully" do
+      @ad.setManager("cline", "leonard").should eql true
+    end
+
+    it "raises an error if it couldn't find the user" do
+      expect{ @ad.setManager("falseuser", "trueuser")}.to raise_error
+    end
+  end
+
+  describe ".getDN" do
+    it "returns a valid DN" do
+      @ad.getDN("cline").should 
+        eql "CN=Alex Cline,CN=Users,DC=bigdatalab,DC=ibm,DC=com"
+    end
+
+    it "raises an error when the user doesn't exist" do
+      expect { @ad.getDN("falseuser") }.to raise_error
     end
   end
 
   describe ".getManager" do
-    it "returns nothing until the manager field is populated"  do
-      @lookup.getManager("cline").should eql nil
+    it "returns the email of the manager"  do
+      @ad.getManager("cline").should eql "rleonard@us.ibm.com"
     end
 
     it "returns nil when the user doesn't have a manager specified" do
-      @lookup.getManager("PassManager").should eql nil
+      @ad.getManager("PassManager").should eql nil
     end
 
+    it "raises an error when the user doesn't exist" do
+      expect { @ad.getManager("falseuser") }.to raise_error
+    end
   end
 
   describe ".getMail" do
     it "returns a valid email address" do
-      @lookup.getMail("cline").should eql "acline@us.ibm.com"
+      @ad.getMail("cline").should eql "acline@us.ibm.com"
     end
 
     it "raises an error when the user doesn't exist" do
-      expect { @lookup.getMail("falseuser") }.to raise_error
+      expect { @ad.getMail("falseuser") }.to raise_error
     end
 
     it "returns nil when the user doesn't have an email address" do
-      @lookup.getMail("PassManager").should eql nil
+      @ad.getMail("PassManager").should eql nil
     end
   end
   
   describe ".getSerial" do
     it "returns a valid serial number" do
-      @lookup.getSerial("cline").should eql "1G4959897"
+      @ad.getSerial("cline").should eql "1G4959897"
     end
 
     it "raises an error when the user doesn't exist" do
-      expect { @lookup.getSerial("falseuser") }.to raise_error
+      expect { @ad.getSerial("falseuser") }.to raise_error
     end
 
     it "returns nil when the user doesn't have a serial number" do
-      @lookup.getSerial("PassManager").should eql nil
+      @ad.getSerial("PassManager").should eql nil
     end
   end
 
-  describe ".userFiler" do
+  describe ".makeFiler" do
     it "returns a filter for one user" do
-      @lookup.userFilter('cline').should 
+      @ad.makeFilter('cline', 'sAMAccountName').should 
         eql "(&(objectClass=person)(!(objectClass=computer))\
           (!(userAccountControl:1.2.840.113556.1.4.803:=2))\
           (sAMAccountName=cline))"
