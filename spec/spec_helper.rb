07f4efa7d2bbe292d5f11e2214ba01a99b3843c0
Alex Cline <acline@us.ibm.com>
2013-05-21 17:33:34 -0400

    Added test logging to spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 dir = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift File.join(dir, 'lib')
+
+require 'logging'
+
+# Set the logging to the test log.
+Logging.output = File.expand_path('../../log/test.log', __FILE__)
